800b5c8310967b14a6b82486de832d922a3d5a57
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-05 07:57:18 +0300

    Add Quasi (QP) and Proportional variants with Nerd Font builds

--- flake.nix
+++ flake.nix
@@ -15,42 +15,51 @@
       (
         system: let
           pkgs = nixpkgs.legacyPackages.${system};
-          plainPackage = pkgs.iosevka.override {
-            set = "neg";
-            privateBuildPlan = builtins.readFile ./iosevka-neg.toml;
+          buildPlan = builtins.readFile ./iosevka-neg.toml;
+
+          mkPlain = set: pkgs.iosevka.override {
+            inherit set;
+            privateBuildPlan = buildPlan;
           };
 
-          nerdFontPackage =
-            pkgs.runCommand "iosevka-nerd-font-${plainPackage.version}" {
-              buildInputs = [pkgs.nerd-font-patcher];
-            } ''
-              set -euo pipefail
-              outDir="$out/share/fonts/truetype"
+          mkNerd = plain: pkgs.runCommand "iosevka-nerd-font-${plain.version}" {
+            buildInputs = [pkgs.nerd-font-patcher];
+          } ''
+            set -euo pipefail
+            outDir="$out/share/fonts/truetype"
 
-              mkdir -p "$outDir"
+            mkdir -p "$outDir"
 
-              for fontfile in ${plainPackage}/share/fonts/truetype/*; do
-                nerd-font-patcher "$fontfile" \
-                  --complete -s --makegroups '-1' --careful \
-                  --outputdir "$outDir" &
-              done
-              wait
-            '';
+            for fontfile in ${plain}/share/fonts/truetype/*; do
+              nerd-font-patcher "$fontfile" \
+                --complete -s --makegroups '-1' --careful \
+                --outputdir "$outDir" &
+            done
+            wait
+          '';
+
+          plainMono = mkPlain "";
+          plainQuasi = mkPlain "quasi";
+          plainProp = mkPlain "prop";
 
           packages = {
-            normal = plainPackage;
-            nerd-font = nerdFontPackage;
+            normal = plainMono;
+            quasi = plainQuasi;
+            prop = plainProp;
+            nerd-font = mkNerd plainMono;
+            nerd-font-quasi = mkNerd plainQuasi;
+            nerd-font-prop = mkNerd plainProp;
           };
         in {
           inherit packages;
-          defaultPackage = nerdFontPackage;
+          defaultPackage = packages.nerd-font;
         }
       );
   in {
     packages = allSystems.packages;
     defaultPackage = allSystems.defaultPackage;
     overlay = final: prev: {
-      iosevka = allSystems.packages.${final.system}; # either `normal` or `nerd-font`
+      iosevka = allSystems.packages.${final.system};
     };
   };
 }
